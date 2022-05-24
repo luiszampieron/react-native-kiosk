927341e3306f0fc654dbba82580351ea3645e84b
Luis Zampieron <luisfernando@digisat.com.br>
2022-05-24 08:15:58 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-kiosk
+
+a simple kiosk mode in react-native
+
+## Installation
+
+```sh
+npm install react-native-kiosk
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-kiosk";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativekiosk/KioskModule.java
@@ -0,0 +1,34 @@
+package com.reactnativekiosk;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = KioskModule.NAME)
+public class KioskModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "Kiosk";
+
+    public KioskModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
